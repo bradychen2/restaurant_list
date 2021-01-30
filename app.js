96a6b007fdff5ef0d1f31e42ac98e3171cedb40e
bradychen <bradychen2@gmail.com>
2021-01-30 21:30:49 +0800

    feature: create new restaurant / fix: edit, font
    Create new feature of submitting new restaurant.
    Fix showing of font by adding font-awsome CDN.
    Fix edit function by linking to mongodb.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import modules and files
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 require('./config/mongoose')
 const Restaurant = require('./models/restaurant')
 
@@ -14,6 +15,7 @@ app.set('view engine', 'handlebars')
 
 // set static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // route setting for index
 app.get('/', (req, res) => {
@@ -26,32 +28,50 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// route setting for showing details
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant_id = req.params.restaurant_id
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === restaurant_id)
-  res.render('show', { restaurant: restaurant, stylesheet: 'show' })
-})
-
-// route setting for search
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    // accept searching with names or categories
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      restaurant.category.toLocaleLowerCase().includes(keyword.toLowerCase())
-  })
-
-  res.render('index', { restaurants: restaurants, keyword: keyword, stylesheet: 'index' })
-})
-
 // Go to create page
-app.get('/restaurant/new', (req, res) => {
+app.get('/restaurants/new', (req, res) => {
   res.render('new', { stylesheet: 'new' })
 })
 
 // Create new restaurant
+app.post('/restaurants', (req, res) => {
+  const restaurant = req.body
+  console.log(restaurant)
+  return Restaurant.create({
+    name: restaurant.name,
+    name_en: restaurant.name_en,
+    category: restaurant.category,
+    rating: restaurant.rating,
+    location: restaurant.location,
+    phone: restaurant.phone,
+    google_map: restaurant.google_map,
+    image: restaurant.image,
+    description: restaurant.description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
+// route setting for showing details
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant, stylesheet: 'show' }))
+    .catch(error => console.log(error))
+})
+
+// route setting for search
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   const restaurants = restaurantList.results.filter(restaurant => {
+//     // accept searching with names or categories
+//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+//       restaurant.category.toLocaleLowerCase().includes(keyword.toLowerCase())
+//   })
+
+//   res.render('index', { restaurants: restaurants, keyword: keyword, stylesheet: 'index' })
+// })
 
 // server listening
 app.listen(port, () => {
