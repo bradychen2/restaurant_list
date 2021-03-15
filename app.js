ff4f369a3db1c3224657dd0d5c8d4303722199f6
bradychen <bradychen2@gmail.com>
2021-03-15 19:24:41 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 require('./config/mongoose')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
@@ -25,12 +26,14 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
-
+app.use(flash())
 usePassport(app)
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -18,11 +18,26 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都為必填！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email
+    })
+  }
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('User already exists.')
+        errors.push({ message: '此 Email 已經註冊。' })
         res.render('register', {
+          errors,
           name,
           email
         })
@@ -44,6 +59,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout() // Passport logout func
+  req.flash('success_msg', '你已經成功登出！')
   res.redirect('/users/login')
 })
 
