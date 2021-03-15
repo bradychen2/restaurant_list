20f125fc6ce1c1b185b99ce825df90655ac01508
bradychen <bradychen2@gmail.com>
2021-03-15 10:27:56 +0800

    feat: add users module & refac router

---
+++ routes/modules/users.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// router.post('/login')
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// router.post('/register')
+
+router.get('logout', (req, res) => {
+  res.redirect('/users/login')
+})
+
+module.exports = router
