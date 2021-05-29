e770016c13fd14a82cc23a02b38aed6ec130ea13
bradychen <bradychen2@gmail.com>
2021-05-29 22:58:04 +0800

    feature: login error messages

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,4 @@
 const passport = require('passport')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 const LocalStrategy = require('passport-local').Strategy
 const FaceBookStrategy = require('passport-facebook').Strategy
 const User = require('../models/user')
@@ -11,21 +8,23 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email })
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'Email is not registered!' })
-        }
-        bcrypt.compare(password, user.password).then(isMatch => {
-          if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect!' })
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true },
+    (req, email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, req.flash('error_msg', '此 email 未註冊!'))
+          } else {
+            bcrypt.compare(password, user.password).then(isMatch => {
+              if (!isMatch) {
+                return done(null, false, req.flash('error_msg', 'Email 或密碼不正確!'))
+              }
+              return done(null, user)
+            }).catch(err => done(err, false))
           }
-          return done(null, user)
         })
-      })
-      .catch(err => done(err, false))
-  }))
+        .catch(err => done(err, false))
+    }))
 
   passport.use(new FaceBookStrategy({
     clientID: process.env.FACEBOOK_ID,
