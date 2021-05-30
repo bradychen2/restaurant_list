c42dcdf28095f5bc81cba2d6416d53384b0e5e7a
bradychen <bradychen2@gmail.com>
2021-05-30 17:24:15 +0800

    refac: user signUp async await

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,48 +11,33 @@ const userController = {
     res.render('register')
   },
 
-  signUp: (req, res) => {
+  signUp: async (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     const errors = []
+    // blank input
     if (!email || !password || !confirmPassword) {
       errors.push({ message: 'Email和密碼欄位為必填！' })
     }
+    // confirm password wrong
     if (password !== confirmPassword) {
       errors.push({ message: '密碼與確認密碼不符！' })
     }
     if (errors.length) {
-      return res.render('register', {
-        errors,
-        name,
-        email
-      })
+      return res.render('register', { errors, name, email })
+    } else {
+      const user = await User.findOne({ email })
+      // email already exists
+      if (user) {
+        errors.push({ message: '此 Email 已經註冊。' })
+        return res.render('register', { errors, name, email })
+      }
+      // create new user
+      const salt = await bcrpyt.genSalt(10)
+      const hash = await bcrpyt.hash(password, salt)
+      await User.create({ name, email, password: hash })
+
+      return res.redirect('/users/login')
     }
-    User.findOne({ email })
-      .then(user => {
-        if (user) {
-          errors.push({ message: '此 Email 已經註冊。' })
-          return res.render('register', {
-            errors,
-            name,
-            email
-          })
-        } else {
-          return bcrpyt
-            .genSalt(10)
-            .then(salt => bcrpyt.hash(password, salt))
-            .then(hash => User.create({
-              name,
-              email,
-              password: hash
-            }))
-            .then(() => {
-              res.redirect('/users/login')
-            })
-            .catch(err => {
-              console.log(err)
-            })
-        }
-      })
   },
 
   logout: (req, res) => {
